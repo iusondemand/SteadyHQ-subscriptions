68169b80e33bcbb6ac2606dc108ef0e8ab4b0dc9
Valentino Spataro <spataro@iusseek.eu>
2025-02-01 20:46:37 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+# remember to config it using integration API in Steadyhq. Every secret is different for every podcast.
+
+
+$clientId = 'xxx';
+$clientSecret = 'xxx';
+$redirectUri = 'https://xxx/callback.php'; // Update this to your redirect URI
